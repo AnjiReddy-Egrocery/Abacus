51137a9f8dd968eb43249a7a2289ff38996edaf3
ADMIN <adityaraj140e@gmail.com>
2024-12-10 14:38:38 +0530

    hi

--- app/src/main/java/com/dst/abacustrainner/SpalashActivity.java
+++ app/src/main/java/com/dst/abacustrainner/SpalashActivity.java
@@ -13,11 +13,10 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-import com.dst.abacustrainner.User.LoginActivity;
+import com.dst.abacustrainner.User.WelcomeActivity;
 
 import java.util.Timer;
 import java.util.TimerTask;
-
 
 public class SpalashActivity extends AppCompatActivity {
     private static  int SPLASH_SCREEN=3000;
@@ -48,7 +47,7 @@ public class SpalashActivity extends AppCompatActivity {
             public void run() {
                 try {
                     sleep(SPLASH_SCREEN);
-                    Intent main = new Intent(SpalashActivity.this, LoginActivity.class);
+                    Intent main = new Intent(SpalashActivity.this, WelcomeActivity.class);
                     startActivity(main);
                 }
 
